1d1cae2125f2883b58efceecc0cfa3344a5ad76c
Erlein <einmoerlend@gmail.com>
2022-02-23 16:03:54 +0100

    Fixed exec.sh

--- binaries/raspbian/armv7l/exec.sh
+++ binaries/raspbian/armv7l/exec.sh
@@ -1,3 +1,3 @@
 PYTHONPATH=$PYTHONPATH:.:../../../python \
 LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH \
-python3 ../../../pythondev/test.py -i ../../../imgs
+python3 ../../../pythondev/timedDirDet.py -i ../../../imgs
