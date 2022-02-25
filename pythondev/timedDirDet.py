968d5a0a49f39dba0c6fedc2ff2cf5d82f1bf380
Erlein <einmoerlend@gmail.com>
2022-02-25 15:20:25 +0100

    Added writing of results to (csv) file

--- pythondev/timedDirDet.py
+++ pythondev/timedDirDet.py
@@ -10,6 +10,7 @@ begin = time.process_time()
 # Argument parsing
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input",	dest="input",	type=str,	required=True,	help="Input directory path (required).")
+ap.add_argument("-o", "--output",	dest="outDir",	type=str,	required=False,	default="results.csv", 	help="Output filename(path).")
 args = ap.parse_args()
 
 # Check and walk directory.
@@ -30,11 +31,16 @@ CONFIG = {
 # Set img format (TODO: implement checking, if needed)
 format = alpr.ULTALPR_SDK_IMAGE_TYPE_RGB24
 
+# Open/create file for result outputs.
+outFile = os.open(args.outDir, os.O_CREAT|os.O_RDWR)
+
 # Engine init
 #checkResult("Init", alpr.UltAlprSdkEngine_init(json.dumps(CONFIG)))
 alpr.UltAlprSdkEngine_init(json.dumps(CONFIG))
 it = time.process_time() - begin
 print("Init time taken:", it)
+itStr = str(it) + "\n"
+os.write(outFile, (str.encode(itStr)))
 
 # Read directory and establish loop
 path = os.walk(args.input)	# Walk directory.
@@ -56,11 +62,17 @@ for r, dir, files in path:
 		print(outson['plates'][0]['text'])
 		print("Detection time taken:", tt)
 
+		# Create output as string and write to file.
+		outStr = file + "," + outson['plates'][0]['text'] + "," + str(tt) + "\n"
+		os.write(outFile, str.encode(outStr))
+
+print("") # Output-spacer
 # Pause
 # input("\nPress enter to deinit engine and exit program.")
 
-# Engine deinit
+# Engine deinit and close file
 alpr.UltAlprSdkEngine_deInit()
+os.close(outFile) # Close output file.
 
 # EOF
 print("Seems like it worked!")
