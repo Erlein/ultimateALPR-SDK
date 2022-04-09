147941e4d0218357e011d1e8c46f6022ca7836be
Erlein <einmoerlend@gmail.com>
2022-04-09 14:29:34 +0200

    Added more statistics and error detection in cases where plate is not detected.

--- binaries/raspbian/armv7l/exec.sh
+++ binaries/raspbian/armv7l/exec.sh
@@ -1,3 +1,8 @@
+IMGPATH="${1:-$../../../imgs}"
+RESPATH="${2:-$'results.csv'}"
+
+echo "Image path: $IMGPATH, Result path: $RESPATH"
+
 PYTHONPATH=$PYTHONPATH:.:../../../python \
 LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH \
-python3 ../../../pythondev/timedDirDet.py -i ../../../imgs
+python3 ../../../pythondev/timedDirDet.py -i $IMGPATH -o $RESPATH
--- pythondev/timedDirDet.py
+++ pythondev/timedDirDet.py
@@ -37,36 +37,53 @@ outFile = os.open(args.outDir, os.O_CREAT|os.O_RDWR)
 # Engine init
 #checkResult("Init", alpr.UltAlprSdkEngine_init(json.dumps(CONFIG)))
 alpr.UltAlprSdkEngine_init(json.dumps(CONFIG))
-it = time.process_time() - begin
+it = round(time.process_time() - begin, 3)
 print("Init time taken:", it)
 itStr = str(it) + "\n"
 os.write(outFile, (str.encode(itStr)))
+
+# Variables for score-keeping
+imgCnt = 0
+dets = 0
 
 # Read directory and establish loop
 path = os.walk(args.input)	# Walk directory.
 for r, dir, files in path:
 	for file in files:
-		# Load image and gather relevant info
-		img = Image.open(os.path.join(args.input, file))	# Load image
-		width, height = img.size							# Get image size.
-		start = time.process_time()							# Start timing for LPR
+		if ".jpg" in file:
+			imgCnt += 1		# Iterate image counter for each image
+			# Load image and gather relevant info
+			img = Image.open(os.path.join(args.input, file))	# Load image
+			width, height = img.size							# Get image size.
+			start = time.process_time()							# Start timing for LPR
 
-		# Perform LPR
-		out = alpr.UltAlprSdkEngine_process(format, img.tobytes(), width, height, 0)
-		tt = time.process_time() - start					# Calcualte and log time for LPR
+			# Perform LPR
+			out = alpr.UltAlprSdkEngine_process(format, img.tobytes(), width, height, 0)
+			tt = round(time.process_time() - start, 3)					# Calcualte and log time for LPR
 
-		# Parse JSON output and output
-		output = out.json()
-		outson = json.loads(output)
-		print("\n" + file)
-		print(outson['plates'][0]['text'])
-		print("Detection time taken:", tt)
+			# Parse JSON output and output
+			output = out.json()
+			outson = json.loads(output)
+			print("\n" + file)
+			print("Detection time taken:", tt)
 
-		# Create output as string and write to file.
-		outStr = file + "," + outson['plates'][0]['text'] + "," + str(tt) + "\n"
-		os.write(outFile, str.encode(outStr))
+			# Create output as string and write to file.
+			if 'plates' in outson:	# First check if plate was sucessfully detected
+				dets += 1				# Iterate detection counter
+				print(outson['plates'][0]['text'])
+				outStr = file + "," + outson['plates'][0]['text'] + "," + str(tt) + "\n"
+			else:
+				print("No plate found!")
+				outStr = file + "," + "NoPlateFound," + str(tt) + "\n"
 
-print("") # Output-spacer
+			# print("Detection time taken:", tt)
+			os.write(outFile, str.encode(outStr))
+
+# Printing statistics for entire run
+print("\nTotal images:", imgCnt) # Output-spacer
+print("Plates detected:", dets)
+print("Proportion:" ,str(round((dets / imgCnt)*100,3)) + "%\n")
+
 # Pause
 # input("\nPress enter to deinit engine and exit program.")
 
